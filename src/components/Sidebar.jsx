de2359e95ff2da881fae6ae54c9d3e7d64a40e00
KeilaBarreto <keilakdbweb@gmail.com>
2024-05-31 11:47:34 -0300

    Sidebar finalizado

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,14 @@
 import Avatar from '../img/eu.jpeg'
 import "../styles/components/sidebar.sass";
+import InformationContainer from './InformationContainer';
+import SocialNetWorks from './SocialNetWorks';
 
 const Sidebar = () => {
     return <aside id="sidebar">
         <img src={Avatar} alt="Keila Barreto"/>
         <p className="title">Desenvolvedora</p>
-        <p>redes sociais</p>
-        <p>informações de contato</p>
+        <SocialNetWorks/>
+        <InformationContainer/>
         <a href="" className="btn">Download curriculo</a>
         </aside>;
 }
